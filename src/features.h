9ed2bf430fe7208284944198e20109c9f8d80284
Barahlush <baraltiva@gmail.com>
2017-08-16 20:11:03 +0300

    Master cleared, go and ju forks are created

--- src/features.h
+++
@@ -1,3 +0,0 @@
-extern double getAverageColor(cv::Mat& gray, cv::Point center);
-extern std::pair<cv::Point, cv::Point> getTargetVector(cv::Mat& gray, std::vector<cv::Vec3f> circles);
-extern void processImage(cv::Mat&);
